f1368ed5396b2066b828dbd84f615a5dbf2b9619
mdyousufniaz <mdyousufniaz@gmail.com>
2025-09-20 10:40:57 +0600

    make student Map final and fixed some printing

--- main.dart
+++ main.dart
@@ -1,17 +1,17 @@
 void main() {
-  Map<String , Object> student = {
+  final Map<String, Object> student = {
 		'name': 'Alice',
 		'age': 20,
 		'grade': 'A'
   };
 
 	print("Name: ${student['name']}");
-	print("age: ${student['age']}");
+	print("Age: ${student['age']}");
 
 	student['age'] = 21;
 	student['grade'] = 'A+';
 	student['school'] = "Greenwood High";
 
-	print("\nPrinting 'Student' map:");
+	print("\nPrinting 'Student' Details:");
 	student.forEach((key, value) => print("$key: $value"));
 }
